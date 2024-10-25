c3058ac4aca9f29f65e9b18500539d2a4697d145
constantino <cons_benjamin9@outlook.com>
2024-10-24 21:05:25 -0300

    .

--- frontend/pages/admin/suscriptores.js
+++ frontend/pages/admin/suscriptores.js
@@ -1,21 +1,43 @@
-import fs from "fs";
-import path from "path";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Pagination from "../../../frontend/components/Pagination";
 
-export default function Suscriptores({ emails }) {
+export default function Suscriptores() {
   const [currentPage, setCurrentPage] = useState(1);
+  const [suscriptores, setSuscriptores] = useState([]);
   const emailsPerPage = 39;
   const indexOfLastEmail = currentPage * emailsPerPage;
   const indexOfFirstEmail = indexOfLastEmail - emailsPerPage;
-  const currentEmails = emails.slice(indexOfFirstEmail, indexOfLastEmail);
-  const totalPages = Math.ceil(emails.length / emailsPerPage);
+  const currentEmails = suscriptores.slice(indexOfFirstEmail, indexOfLastEmail); 
+  const totalPages = Math.ceil(suscriptores.length / emailsPerPage); 
+
+  const obtenerSuscriptores = async () => {
+    try {
+      const response = await fetch("https://web-production-73e61.up.railway.app/api/suscriptores", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) {
+        throw new Error("Error al obtener los suscriptores");
+      }
+      const data = await response.json();
+      setSuscriptores(data);
+    } catch (error) {
+      console.error("Error al obtener los suscriptores:", error);
+    }
+  };
+
+  
+  useEffect(() => {
+    obtenerSuscriptores();
+  }, []);
 
   return (
     <div className="container p-4 mx-auto">
       <h1 className="mb-4 text-2xl font-bold">Lista de Suscriptores</h1>
       <div className="p-4 bg-white rounded-lg shadow-md">
-        {emails.length > 0 ? (
+        {suscriptores.length > 0 ? (
           <div className="grid grid-cols-3 gap-4">
             {currentEmails.map((email, index) => (
               <div
@@ -30,7 +52,7 @@ export default function Suscriptores({ emails }) {
           <p className="text-gray-500">No hay suscriptores.</p>
         )}
       </div>
-      {emails.length > emailsPerPage && (
+      {suscriptores.length > emailsPerPage && ( 
         <Pagination
           totalPages={totalPages}
           currentPage={currentPage}
@@ -39,20 +61,4 @@ export default function Suscriptores({ emails }) {
       )}
     </div>
   );
-}
-
-export async function getStaticProps() {
-  const filePath = path.join(process.cwd(), "suscriptores.json");
-  let emails = [];
-  try {
-    const jsonData = fs.readFileSync(filePath, "utf-8");
-    emails = JSON.parse(jsonData || "[]");
-  } catch (error) {
-    console.error("Error al leer el archivo:", error);
-  }
-  return {
-    props: {
-      emails,
-    },
-  };
 }
