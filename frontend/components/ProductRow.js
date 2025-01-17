b7d709f1e75a6e71b96c3068fa37486db40586a2
Sainte0 <s.santo2004@gmail.com>
2025-01-16 23:22:37 -0300

    test fix 12

--- frontend/components/ProductRow.js
+++ frontend/components/ProductRow.js
@@ -47,11 +47,10 @@ const ProductRow = ({
       setNewTalla("");
       setNewStock("");
     } else {
-      alert(
-        "Por favor ingresa un nombre de talla válido y precio de la Talla."
-      );
+      alert("Por favor ingresa un nombre de talla válido y precio de la Talla.");
     }
   };
+
   const handleDeleteTalla = (tallaIndex) => {
     const updatedProducts = [...editableProducts];
     updatedProducts[index].tallas.splice(tallaIndex, 1);
@@ -60,14 +59,13 @@ const ProductRow = ({
 
   const handleColorChange = (e, colorId) => {
     const updatedProducts = [...editableProducts];
-    const colorIndex = updatedProducts[index].colores.findIndex(
-      (c) => c._id === colorId
-    );
+    const colorIndex = updatedProducts[index].colores.findIndex((c) => c._id === colorId);
     if (colorIndex !== -1) {
       updatedProducts[index].colores[colorIndex].color = e.target.value;
       setEditableProducts(updatedProducts);
     }
   };
+
   const handleAddColor = () => {
     if (newColor) {
       const updatedProducts = [...editableProducts];
@@ -78,11 +76,10 @@ const ProductRow = ({
       alert("Por favor ingresa un color válido.");
     }
   };
+
   const handleDeleteColor = (colorId) => {
     const updatedProducts = [...editableProducts];
-    updatedProducts[index].colores = updatedProducts[index].colores.filter(
-      (c) => c._id !== colorId
-    );
+    updatedProducts[index].colores = updatedProducts[index].colores.filter((c) => c._id !== colorId);
     setEditableProducts(updatedProducts);
   };
 
@@ -90,7 +87,7 @@ const ProductRow = ({
     const updatedProduct = {
       ...producto,
       categoria: producto.categoria.toLowerCase(), // Normaliza a minúsculas
-    };    
+    };
     const formData = new FormData();
     Object.keys(updatedProduct).forEach((key) => {
       if (key !== "image") {
@@ -110,16 +107,13 @@ const ProductRow = ({
       if (newImage) {
         const imageFormData = new FormData();
         imageFormData.append("image", newImage);
-        const imageResponse = await fetch(
-          `${URL1}/api/productos/${producto._id}/image`,
-          {
-            method: "PUT",
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem("token")}`,
-            },
-            body: imageFormData,
-          }
-        );
+        const imageResponse = await fetch(`${URL1}/api/productos/${producto._id}/image`, {
+          method: "PUT",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+          body: imageFormData,
+        });
         if (imageResponse.ok) {
           toast.success("Imagen actualizada con éxito");
           await fetchProducts();
@@ -137,9 +131,7 @@ const ProductRow = ({
   };
 
   const handleProductDelete = async (id) => {
-    const confirmDelete = window.confirm(
-      "¿Estás seguro que quieres eliminar este producto?"
-    );
+    const confirmDelete = window.confirm("¿Estás seguro que quieres eliminar este producto?");
     if (confirmDelete) {
       const response = await fetch(`${URL1}/api/productos/${id}`, {
         method: "DELETE",
@@ -181,27 +173,21 @@ const ProductRow = ({
   const handleDestacadoChange = (e) => {
     const updatedProduct = { ...producto, destacado: e.target.checked };
     setEditableProducts((prevProducts) =>
-      prevProducts.map((prod) =>
-        prod._id === producto._id ? updatedProduct : prod
-      )
+      prevProducts.map((prod) => (prod._id === producto._id ? updatedProduct : prod))
     );
   };
 
   const handleDestacadoZapatillasChange = (e) => {
     const updatedProduct = { ...producto, destacado_zapatillas: e.target.checked };
     setEditableProducts((prevProducts) =>
-      prevProducts.map((prod) =>
-        prod._id === producto._id ? updatedProduct : prod
-      )
+      prevProducts.map((prod) => (prod._id === producto._id ? updatedProduct : prod))
     );
   };
 
   const handleEncargoChange = (e) => {
     const updatedProduct = { ...producto, encargo: e.target.checked };
     setEditableProducts((prevProducts) =>
-      prevProducts.map((prod) =>
-        prod._id === producto._id ? updatedProduct : prod
-      )
+      prevProducts.map((prod) => (prod._id === producto._id ? updatedProduct : prod))
     );
   };
 
@@ -298,14 +284,14 @@ const ProductRow = ({
       <td className="px-2 py-2 border">
         {selectedProduct === producto._id ? (
           <div>
-            {producto.tallas.map((talla, tallaIndex) => (
+            {producto.tallas.map((tallaObj, tallaIndex) => (
               <div
                 key={tallaIndex}
                 className="flex flex-col sm:flex-row items-center mb-2 sm:mb-1"
               >
                 <input
                   type="text"
-                  value={talla.talla}
+                  value={tallaObj.talla}
                   readOnly
                   className="w-full sm:w-1/3 p-1 mb-2 sm:mb-0 sm:mr-2 border"
                 />
