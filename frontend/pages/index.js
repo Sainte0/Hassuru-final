12aae587bf89810cab0259299af28a17381730c1
Sainte0 <s.santo2004@gmail.com>
2025-03-06 00:06:45 -0300

    test 20

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -25,50 +25,9 @@ export default function Home() {
 
   return (
     <main>
-      <div className="container p-4 mx-auto">
-        <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
-          <Link className="w-full md:w-[49.51%] h-auto block" href="/productos/categoria/zapatillas">
-            <Image
-              src="/images/Sneackers.png"
-              alt="Catalogo"
-              width={600}
-              height={500}
-              className="object-cover w-full h-full"
-            />
-          </Link>
-          <Link className="w-full md:w-[50.49%] h-auto block" href="/productos/categoria/ropa">
-            <Image
-              src="/images/Ropa.png"
-              alt="Encargo"
-              width={620}
-              height={500}
-              className="object-cover w-full h-full"
-            />
-          </Link>
-        </div>
-      </div>
-      <div className="mt-2">
-        <Carousell dolarBlue={dolarBlue} products={destacados} title={"Destacados"} />
-      </div>
-      <div className="mt-8 mb-10">
-        <Carousell dolarBlue={dolarBlue} products={zapatillas} title={"Zapatillas"} />
-      </div>
-      <div className="container grid grid-cols-1 gap-4 px-2 mx-auto mt-8 lg:px-24 md:grid-cols-3">
-        {tiktokLinks.slice(0, 3).map((linkObj, index) => (
-          <iframe
-            key={index}
-            src={linkObj.link}
-            width="100%"
-            height="750"
-            style={{ border: "none" }}
-            allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; accelerometer; gyroscope;"
-            allowFullScreen
-          ></iframe>
-        ))}
-      </div>
-      <div className="mb-4">
-        <Newsletter />
-      </div>
+     <div>
+      <h1>hola mundo</h1>
+     </div>
     </main>
   );
 }
