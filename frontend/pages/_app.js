32271f359e2074bd8bb7aedc7f51ced3eecb4100
Sainte <s.santo2004@gmail.com>
2025-04-07 21:09:08 -0300

    analiticas vercel

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import Layout from "@/components/Layout";
 import { Toaster } from 'react-hot-toast';
 import { Roboto } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/react"
 
 const roboto = Roboto({
   weight: ['400', '700'],
@@ -14,6 +15,7 @@ export default function App({ Component, pageProps }) {
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      <Analytics />
     </div>
   )
 }
