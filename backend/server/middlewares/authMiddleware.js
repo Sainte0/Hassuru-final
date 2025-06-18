ad61912bf7c6daa26e3dd160484b8fc05a78eca7
Sainte <s.santo2004@gmail.com>
2025-06-18 14:16:16 -0300

    empty

--- backend/server/middlewares/authMiddleware.js
+++ backend/server/middlewares/authMiddleware.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const authMiddleware = (req, res, next) => {
   const token = req.headers['authorization']?.split(' ')[1];
   if (!token) {
-    return res.status(403).json({ error: 'Token no proporcionado' });
+    return res.status(403).json({ error: 'Token no proporcionado ' });
   }
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
