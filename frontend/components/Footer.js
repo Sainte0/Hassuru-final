14a4b25060cd22ed06edd39a8ce979f312b45185
Sainte <s.santo2004@gmail.com>
2025-07-04 21:10:50 -0300

    fix fondo 3

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -9,8 +9,8 @@ export default function Footer() {
   };
 
   return (
-    <footer className="p-1 text-black dark:text-white bg-white dark:bg-gray-800 border-t-2 border-black dark:border-gray-700 shadow-md transition-colors duration-300">
-      <div className="container flex flex-col items-center justify-between mx-auto space-y-4 rounded-sm md:flex-row md:space-y-0">
+    <footer className="text-black dark:text-white bg-white dark:bg-gray-800 border-t-2 border-black dark:border-gray-700 shadow-md transition-colors duration-300">
+      <div className="container flex flex-col items-center justify-between mx-auto space-y-4 rounded-sm md:flex-row md:space-y-0 p-4">
         <div className="flex space-x-6">
           <a href="https://www.instagram.com/hassuru.ar/?hl=es" aria-label="Instagram" target="_blank" rel="noopener noreferrer">
             <FaInstagram className="w-6 h-6 hover:text-gray-500 dark:hover:text-gray-400 transition-colors" />
@@ -20,11 +20,11 @@ export default function Footer() {
           </a>
         </div>
         <div className="text-sm text-center md:text-right">
-          <p className='mt-4 text-gray-900 dark:text-gray-300'>Email: hassuru.ar@gmail.com</p>
+          <p className='text-gray-900 dark:text-gray-300'>Email: hassuru.ar@gmail.com</p>
           <button className='text-green-700 dark:text-green-400 hover:text-green-800 dark:hover:text-green-300 transition-colors' onClick={handleSubmit}>Tel: 351 259 5858</button>
         </div>
       </div>
-      <div className="pb-4 mt-4 text-sm text-center text-gray-700 dark:text-gray-400">
+      <div className="text-sm text-center text-gray-700 dark:text-gray-400 pb-4">
         &copy; 2025 @Hassuru. Todos los derechos reservados.
       </div>
     </footer>
