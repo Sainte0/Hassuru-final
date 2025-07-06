68aa8786c785ee1d1eeaf366f75fa7d6ded3c906
Sainte <s.santo2004@gmail.com>
2025-07-06 16:17:24 -0300

    fix fondo 6

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   const showLayout = router.pathname !== '/login' && router.pathname !== '/admin';
 
   return (
-    <div className='flex flex-col min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300'>
+    <div className='flex flex-col h-screen bg-white dark:bg-gray-900 transition-colors duration-300'>
       {showLayout && <Navbar />}
       {showLayout && (
         <div className="w-full bg-white dark:bg-gray-800 shadow-sm px-2 py-4 flex justify-center border-b border-gray-200 dark:border-gray-700">
